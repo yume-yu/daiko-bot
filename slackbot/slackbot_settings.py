e38e920c5abdcbe2769287c53c311e2d2b84d3d2
yume_yu <yu.matchang@gmail.com>
2019-03-15 18:35:10 +0900

    slackbotとして基礎を作成

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-278661428068-448117697143-9tkqO4BD8tSM8rrx7Vfkk98k"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "こんにちは!"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
