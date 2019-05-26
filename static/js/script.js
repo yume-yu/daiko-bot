0a9319ae26cd914211889007b75fca50e7ebd58c
yume_yu <yu.matchang@gmail.com>
2019-05-27 02:32:02 +0900

    [update] cant switch request and contract window
    代行依頼ウィンドウと代行請負ウィンドウがそれぞれ表示切り替えできるようになった

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,30 @@
 let requestbutton = document.getElementById("request");
-let constractbutton = document.getElementById("constract");
+let constractbutton = document.getElementById("contract");
 let cancelbutton = document.getElementById("cancel");
-let select_list_name = document.getElementById("requestWindow").getElementsByTagName("select")
+let request_list = document.getElementById("request-form").getElementsByTagName("select")
+let contract_list = document.getElementById("contract-form").getElementsByTagName("select")
 
 function toggle_window(){
-  document.getElementById("requestWindow").classList.toggle("hidden-item")
-  select_list_name[1].selectedIndex = 0
-  select_list_name[0].selectedIndex = 0
+  document.getElementById("window").classList.toggle("hidden-item")
+  request_list[1].selectedIndex = 0
+  request_list[0].selectedIndex = 0
+  contract_list[0].selectedIndex = 0
 }
 
-requestbutton.addEventListener("click",toggle_window,false)
+function active_request(){
+  toggle_window()
+  document.getElementById("request-form").classList.remove("hidden-item")
+  document.getElementById("contract-form").classList.add("hidden-item")
+}
+
+function active_contract(){
+  toggle_window()
+  document.getElementById("request-form").classList.add("hidden-item")
+  document.getElementById("contract-form").classList.remove("hidden-item")
+}
+
+requestbutton.addEventListener("click",active_request,false)
+constractbutton.addEventListener("click",active_contract,false)
 cancelbutton.addEventListener("click",toggle_window,false)
 
 fetch("_get_members").then(function(response){
@@ -18,19 +33,19 @@ fetch("_get_members").then(function(response){
   for(var item in json){
     var temp_option = document.createElement('option')
     temp_option.textContent = json[item];
-    select_list_name[0].appendChild(temp_option)
+    request_list[0].appendChild(temp_option)
   }
 })
-select_list_name[0].addEventListener("change",function(){
+request_list[0].addEventListener("change",function(){
   //子要素を全て削除
-  for(var item in [...Array(select_list_name[1].children.length).keys()]){
+  for(var item in [...Array(request_list[1].children.length).keys()]){
     console.log(item)
     if(item != 0){
-        select_list_name[1].removeChild(select_list_name[1].children[1])
+        request_list[1].removeChild(request_list[1].children[1])
     }
   }
   fetch("_get_of_member",{method:"POST",headers : { "Content-type" : "application/json; charset=utf-8" },
-    body : JSON.stringify({name: select_list_name[0].value})}).then(function(response){
+    body : JSON.stringify({name: request_list[0].value})}).then(function(response){
       return response.json()
     }).then(function(json){
       console.log(json)
@@ -43,7 +58,7 @@ select_list_name[0].addEventListener("change",function(){
           console.log(Object.keys(json[item]) + ":" + json[item][Object.keys(json[item])][time]["start"] + "~" + json[item][Object.keys(json[item])][time]["end"])
           temp_option.textContent = Object.keys(json[item]) + ":" + json[item][Object.keys(json[item])][time]["start"] + "~" + json[item][Object.keys(json[item])][time]["end"]
           temp_option.value = item+ ":" + time
-          select_list_name[1].appendChild(temp_option)
+          request_list[1].appendChild(temp_option)
         }
       }
     })
