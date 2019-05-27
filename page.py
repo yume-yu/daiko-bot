f7a5879e10668afd5a7acb87c41f68b05956c2cc
yume_yu <yu.matchang@gmail.com>
2019-05-28 01:02:35 +0900

    [update] implement request flow
    各所を通して依頼部分の実装終わり

--- page.py
+++ page.py
@@ -10,23 +10,13 @@ from workmanage import DrawShiftImg, Shift
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
-# shift = Shift.parse_json("./shift.json")
-shift = Shift.parse_json("./sample.json")
+shift = Shift.parse_json("./shift.json")
+# shift = Shift.parse_json("./sample.json")
 
 
 @app.route("/")
 def root_page():
-    global shift
-    make = DrawShiftImg(shift, "/Users/yume_yu/Library/Fonts/Cica-Regular.ttf")
-    image = make.makeImage()
-    filename = (
-        "shiftimage-" + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f") + ".jpg"
-    )
-    filepath = "./static/img/" + filename
-    for file in glob.glob("./static/img/shiftimage-*.jpg", recursive=True):
-        os.remove(file)
-    image.save("./static/img/" + filename, quality=95)
-    # return '<img src="./static/img/' + filename + '">'
+    filepath = update_image()
     return render_template("index.html", imgpath=filepath)
 
 
@@ -46,6 +36,28 @@ def add_shift_page():
     return redirect(url_for("root_page"))
 
 
+@app.route("/request", methods=["POST"])
+def accept_request():
+    if request.method == "POST":
+        if request.json is not None:
+            item = request.json
+            shift.add_request(
+                Shift.exchange_weekdayname(request.json["day"]),
+                request.json["name"],
+                int(request.json["index"]),
+            )
+            return jsonify(item)
+        else:
+            item = request.form
+            print(item["day"])
+            shift.add_request(
+                Shift.exchange_weekdayname(request.form["day"]),
+                request.form["name"],
+                int(request.form["index"]),
+            )
+            return jsonify(item)
+
+
 @app.route("/_get_members", methods=["GET"])
 def return_member():
     return jsonify(shift.get_member())
@@ -53,12 +65,10 @@ def return_member():
 
 @app.route("/_get_of_member", methods=["GET", "POST"])
 def return_shift_of_member():
-    # return jsonify(
     if request.method == "GET":
         return str(
             shift.get_shift_of_member(urllib.parse.unquote(request.args.get("name")))
         )
-    # )
     elif request.method == "POST":
         if request.json is not None:
             item = shift.get_shift_of_member(urllib.parse.unquote(request.json["name"]))
@@ -75,6 +85,21 @@ def return_shift_of_member():
 @app.route("/_get_week", methods=["GET"])
 def return_json():
     return jsonify(ast.literal_eval(str(shift)))  # 文字列→dict→jsonに変換して返す
+
+
+@app.route("/update_image")
+def update_image():
+    global shift
+    make = DrawShiftImg(shift, "/Users/yume_yu/Library/Fonts/Cica-Regular.ttf")
+    image = make.makeImage()
+    filename = (
+        "shiftimage-" + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f") + ".jpg"
+    )
+    filepath = "./static/img/" + filename
+    for file in glob.glob("./static/img/shiftimage-*.jpg", recursive=True):
+        os.remove(file)
+    image.save(filepath, quality=95)
+    return filepath
 
 
 app.run(debug=True, host="0.0.0.0")
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,11 @@
 let requestbutton = document.getElementById("request");
 let constractbutton = document.getElementById("contract");
 let cancelbutton = document.getElementById("cancel");
+let submitbutton = document.getElementById("submit");
 let request_list = document.getElementById("request-form").getElementsByTagName("select")
 let contract_list = document.getElementById("contract-form").getElementsByTagName("select")
+
+
 
 function toggle_window(){
   document.getElementById("window").classList.toggle("hidden-item")
@@ -24,6 +27,54 @@ function active_contract(){
   document.getElementById("contract-form").classList.remove("hidden-item")
 }
 
+function send_request(){
+  fetch("_get_of_member",{method:"post",headers : { "content-type" : "application/json; charset=utf-8" },
+    body : JSON.stringify({name: request_list[0].value})}).then(function(response){
+      return response.json()
+    }).then(function(json){
+      let shift = json
+      console.log(shift)
+      date  = JSON.stringify(
+        {
+          day:Object.keys(json[request_list._shift.value.split(":")[0]])[0],
+          name:request_list._name.value,
+          index:Number(request_list._shift.value.split(":")[1]) + 1,
+        }
+      )
+      console.log(date)
+      fetch("request",{method:"post",headers : { "content-type" : "application/json; charset=utf-8" },
+        body : date}).then(function(response){
+          return response.json()
+        }).then(function(json){
+          // console.log(json)
+          toggle_window()
+          fetch("update_image").then(function(response){return response.text()}).then(function(text){document.getElementsByTagName("img")[0].src = text;console.log(text)})
+
+        })
+    })
+}
+function send_contract(){
+  fetch("_get_of_member",{method:"post",headers : { "content-type" : "application/json; charset=utf-8" },
+    body : json.stringify({name: request_list[0].value})}).then(function(response){
+      return response.json()
+    }).then(function(json){
+    })
+}
+
+function send_date(){
+  console.log(request_list[1].selectedIndex,contract_list[1].selectedIndex)
+  if (request_list[1].selectedIndex + contract_list[1].selectedIndex == 0){
+    console.log("pass")
+  }else if(request_list[1].selectedIndex * contract_list[1].selectedIndex != 0){
+    console.log("pass")
+  }else if(request_list[1].selectedIndex != 0){
+    //代行依頼送信
+    send_request()
+  }else if(contract_list[1].selectedIndex != 0){
+    send_contract()
+  }
+}
+
 requestbutton.addEventListener("click",active_request,false)
 constractbutton.addEventListener("click",active_contract,false)
 cancelbutton.addEventListener("click",toggle_window,false)
@@ -43,15 +94,17 @@ fetch("_get_members").then(function(response){
   }
 })
 
+submitbutton.addEventListener("click",send_date,false)
+
 request_list[0].addEventListener("change",function(){
   //子要素を全て削除
   for(var item in [...Array(request_list[1].children.length).keys()]){
     console.log(item)
     if(item != 0){
-        request_list[1].removeChild(request_list[1].children[1])
+      request_list[1].removeChild(request_list[1].children[1])
     }
   }
-  fetch("_get_of_member",{method:"POST",headers : { "Content-type" : "application/json; charset=utf-8" },
+  fetch("_get_of_member",{method:"post",headers : { "content-type" : "application/json; charset=utf-8" },
     body : JSON.stringify({name: request_list[0].value})}).then(function(response){
       return response.json()
     }).then(function(json){
